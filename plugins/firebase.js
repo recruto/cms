d7f30c5cca91e66b03a053e84fbd5027c281f972
Diego Leme <diego@alice.com.br>
2020-05-29 01:17:23 -0300

    Config firebase with .env.local

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyCOk_6ScUzhufwGiX0FhJ5sbu8OHnH3m7A',
-  authDomain: 'recru-to.firebaseapp.com',
-  databaseURL: 'https://recru-to.firebaseio.com',
-  projectId: 'recru-to',
-  storageBucket: 'recru-to.appspot.com',
-  messagingSenderId: '373094688113',
-  appId: '1:373094688113:web:e8e2bdca08d7a7876f02d0',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 })
 
 export function signIn() {
